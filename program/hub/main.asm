a723af9a4e3f74c09391effd1b3fecda781db748
Ryan Hardy <ryanjamiehardy@gmail.com>
2025-04-16 21:07:57 +0100

    FROG

--- program/hub/main.asm
+++ program/hub/main.asm
@@ -23,6 +23,40 @@ start:
   mov sp, 0
 
 
+read_input:
+  mov ah, 0h  ; Listen for keys function
+  int 16h     ; Interrupt to set the function into action
+
+  ; Enter key
+  mov si, enter_key
+  cmp al, 0dh
+  je keys
+
+  ; Backspace key
+  mov si, backspace_key
+  cmp al, 08h
+  je keys
+
+  ; Regular keys
+  mov ah, 0eh
+  int 10h
+
+newline:
+  add dh, 1
+  mov dl, 2
+  mov bh, 0
+  mov ah, 2
+  int 10h
 
 loaded_msg: db 'Welcome to the simple_terminal OS hub', 0
-hub_question: db 'What would you like to do (Enter qui for options)?:'
+hub_question: db 'What would you like to do (Enter qui for options)?:', 0
+
+;
+; Keys
+;
+enter_key: db 'Enter', 0
+
+;
+; Commands
+;
+ent: db 'ent', 0
